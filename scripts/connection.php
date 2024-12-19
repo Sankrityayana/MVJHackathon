43a5415b76ec06fd39d7c15d8491af7a3766bffc
Suraj Shanbhag <shanbhagsuraj84@gmail.com>
2024-12-19 10:38:28 +0530

    Update connection.php

---
+++ scripts/connection.php
@@ -0,0 +1,20 @@
+<?php 
+    //host
+    $host = "localhost";
+
+    //dbname
+    $dbname = "lifelineshield";
+
+    //user
+    $user = "root";
+
+    //pass
+    $pass = "";
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;user=$user;pass=$pass");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if($conn) {
+        echo "Connection Established";
+    }
+?>
